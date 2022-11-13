9fe6d2ba2ede138401eb67775609e4fe86419219
beamsbe97 <37875772+beamsbe97@users.noreply.github.com>
2022-11-13 23:58:19 +0800

    Add files via upload

--- AnNguyen_273_A3_FX.java
+++ AnNguyen_273_A3_FX.java
@@ -4,11 +4,152 @@
 // File name: AnNguyen_237_A3.java
 // Declaration: this is my own lab and I have not passed my lab to anyone in this class
 
-class AnNguyen_273_A3_FX{
+import javax.swing.JFrame;
+import javax.swing.JTextField;
 
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+class AnNguyen_273_A3_FX extends JFrame{
+  
     public static void main(String[] args){
-        System.out.println();
+        JFrame what = new JFrame("asdfsdf");
+        DemoSys ds = new DemoSys();
+        ds.setSize(200, 300);
+        ds.setVisible(true);
+        ds.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
     }
+}
+
+class DemoSys extends JFrame{
+    private static Random rand = new Random();
+    private static String[] comments = {"Statements too long", "Use meaningful identifiers", "Overall design OK", "Some improvements needed", "Well done"};
+    private static String[] names = {"Jessica", "Mike", "John", "Boris"};
+    private static String[] titles = {"Part time student", "Full time student"};
+    private static String[] groups = {"T01", "T02", "T03", "T04", "T05"};
+    private static String[] demoWhat = {"Lab 1", "Lab 2", "Lab3", "Lab 4", "Assignment 1", "Assignment 2", "Assignment 3"};
+
+    private static void generateMessage(ArrayList<String> alist){
+        alist.add(comments[rand.nextInt(comments.length)]);
+        for (int i = 0; i < rand.nextInt(comments.length); i++) {
+            String message = comments[rand.nextInt(comments.length)];
+            if(!alist.contains(message) && !alist.contains("Well done")){
+                alist.add(message);
+            }   
+        }
+    }
+
+    public String generateName(){
+        return names[rand.nextInt(names.length)];
+    }
+
+    public String generateTitle(){
+        return titles[rand.nextInt(titles.length)];
+    }
+
+    public String generateGroup(){
+        return groups[rand.nextInt(groups.length)];
+    }
+
+    public String generateDemoWhat(){
+        return demoWhat[rand.nextInt(demoWhat.length)];
+    }
+
+
+
+    private final JButton okButton;
+    private JButton refreshButton;
+
+
+    public DemoSys(){
+        super("Let us start");
+        setLayout (new FlowLayout ());
+        
+        JLabel welcomejl = new JLabel();
+        Icon welcomeic = new ImageIcon("C:/Users/nguye/OneDrive/Desktop/homework.PNG");
+        welcomejl.setIcon(welcomeic);
+        add(welcomejl);
+
+        okButton = new JButton("OK");
+        add(okButton);
+
+        okButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //JOptionPane.showMessageDialog(null,"asdfasdf");
+                JFrame aFrame = new JFrame ("Welcome to 121 Demo System");
+                aFrame.setLayout (new FlowLayout ());
+		        aFrame.setSize (200, 300);
+		        aFrame.setVisible (true);
+
+                JLabel label = new JLabel();
+                Icon ic = new ImageIcon("C:/Users/nguye/OneDrive/Desktop/homework.PNG");
+                label.setIcon(ic);
+                aFrame.add(label);
+                refreshButton = new JButton("Refresh button to get the next student");
+                //aFrame.add(label);
+                aFrame.add(refreshButton);
+
+                refreshButton.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        Student student = new Student(generateName(),generateTitle(), "1.jpg", generateGroup(), generateDemoWhat());
+                        JFrame studentFrame = new JFrame("Let us welcome " + student.getName());
+                        studentFrame.setLayout (new FlowLayout ());
+
+                        JLabel studentLabel = new JLabel(student.toString());
+                        studentFrame.add(studentLabel);
+    
+                        studentFrame.setSize (200, 300);
+                        studentFrame.setVisible (true);
+                        JButton buttonOK1 = new JButton("OK");
+                        studentFrame.add(buttonOK1);
+                        
+
+                        buttonOK1.addActionListener(new ActionListener() {
+                            public void actionPerformed(ActionEvent e) {
+                                ArrayList<String> message = new ArrayList<String>();
+                                generateMessage(message);
+                                Lecturer lecturer = new Lecturer(student.getName(), student.getTitle(), "heng.jpg", message);
+                                JFrame lecturerFrame = new JFrame("Hi "+ student.getName() +", my comment to your " + student.getTitle());
+                                lecturerFrame.setLayout (new FlowLayout ());
+
+                                JLabel lecturerLabel = new JLabel(lecturer.toString());
+                                lecturerFrame.add(lecturerLabel);
+            
+                                lecturerFrame.setSize (200, 300);
+                                lecturerFrame.setVisible (true);
+                                JButton buttonOK2 = new JButton("OK");
+                                lecturerFrame.add(buttonOK2);
+
+                                
+                            }
+                        });
+                    
+                    }
+                });
+                
+
+
+            }
+        });
+
+
+    }
+
 }
 
 class PersonInfo{
@@ -76,7 +217,7 @@ class Student extends PersonInfo{
 
     @Override
     public String toString(){
-        return "";
+        return String.format("<html>I am %s<br>%s<br>I am from group %s<br>I wish to demo %s%n<html>", name, title, group, demoWhat);
     }
 
 }
@@ -104,7 +245,7 @@ class Lecturer extends PersonInfo{
     }
 
     public String toString(){
-        return "";
+        return String.format("<html>My suggestions, if any:<br>%s<html>", message);
     }
 
 }
